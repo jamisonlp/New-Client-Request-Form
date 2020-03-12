e5e1fb02caeeb7bc4a0afee3bd94e3abb46c9d2a
jamisonlp@gmail.com <jamisonlp@gmail.com>
2020-03-11 19:30:59 -0500

    First commit.

---
+++ New Client Request FormWeb/MessageRead.js
@@ -0,0 +1,75 @@
+﻿(function () {
+  "use strict";
+
+  var messageBanner;
+
+  // The Office initialize function must be run each time a new page is loaded.
+  Office.initialize = function (reason) {
+    $(document).ready(function () {
+      var element = document.querySelector('.ms-MessageBanner');
+      messageBanner = new fabric.MessageBanner(element);
+      messageBanner.hideBanner();
+      loadProps();
+    });
+  };
+
+  // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
+  function buildAttachmentsString(attachments) {
+    if (attachments && attachments.length > 0) {
+      var returnString = "";
+      
+      for (var i = 0; i < attachments.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + attachments[i].name;
+      }
+
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Format an EmailAddressDetails object as
+  // GivenName Surname <emailaddress>
+  function buildEmailAddressString(address) {
+    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
+  }
+
+  // Take an array of EmailAddressDetails objects and
+  // build a list of formatted strings, separated by a line-break
+  function buildEmailAddressesString(addresses) {
+    if (addresses && addresses.length > 0) {
+      var returnString = "";
+
+      for (var i = 0; i < addresses.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + buildEmailAddressString(addresses[i]);
+      }
+         
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Load properties from the Item base object, then load the
+  // message-specific properties.
+  function loadProps() {
+    var item = Office.context.mailbox.item;
+      const today = new Date(Date.now());
+      $('#dateCreated').val(today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear());
+      $('#sAtty').val(Office.context.mailbox.userProfile.displayName);
+  }
+
+  // Helper function for displaying notifications
+  function showNotification(header, content) {
+    $("#notificationHeader").text(header);
+    $("#notificationBody").text(content);
+    messageBanner.showBanner();
+    messageBanner.toggleExpansion();
+  }
+})();
